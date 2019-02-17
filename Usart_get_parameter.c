32e71e481556f998f7afc8d2e73eb8c53846c8eb
DesignMyself <379157471@qq.com>
2019-02-17 22:53:32 +0800

    增加了ASCII转换为数值，输入角度值获取

--- Usart_get_parameter.c
+++ Usart_get_parameter.c
@@ -2,10 +2,13 @@
 /*从串口2中获得电脑发过来的数据（PID)参数,PID_Para_thread_entry(void* parameter),任务等级15*/
 /*把获得的串口参数PID_Para[16]转换为PID中的P,I,D值，并赋值给MOTOR2的PID结构体列表*/
 /**二、利用串口3获得大彩屏幕发过来的数据(输入的数字大小），大彩屏幕规律
-		EE B1 11 00 00 00 09 11 00 FF FC FF FF 
-		前六位为开始验证码，09表示屏幕中第九个框图键盘发出来的参数，11表面在第一个screen中发过来的参数，后五位是验证码
-		EE B1 11 00 00 00 09 11 32 33 2E 36 35 00 FF FC FF FF 
-		从11到00之间的数为发过来的参数的ASCII
+		EE B1 11 00 00 00 09 11  45 35 58 FF FC FF FF 
+		
+		返回指令格式： EE B1 11 Screen_id(00 00) Control_id(00 09) Control_type(11) Strings (45 35 58)FF FC FF FF
+返回参数： Screen_id(2 个字节)：画面编号
+Control_id(2 个字节)：控件编号
+Control_type(1 个字节)：固定值 0x11，表示为文本控件
+Strings(不定长)：当前显示的文本值
 三、大彩屏发送命令到控制板
 			EE B1 A1 A1 A1 A1 A1 11 00 FF FC FF FF;//自动前进，此时必须是处于自动模式下才可以，当自动模式断开，就不再自动前进
 			EE B1 A2 A2 A1 A1 A1 11 00 FF FC FF FF;//自动前进，此时必须是处于自动模式下才可以，当自动模式断开，就不再自动前进
@@ -29,13 +32,17 @@
 #include "HDMI.H"
 #include "motor1.h"
 #include "PWM.H"
-	
-	extern PID Motor1_PID;//电机1PID参数初始化
-	extern PID Motor2_PID;//电机2PID参数初始化
+	static rt_thread_t Angle_Get_Data=RT_NULL;//获获取MPC6550的数据
+	static rt_thread_t Usart_Remote_Data=RT_NULL;//获取遥控发过来的数据
+	static rt_thread_t Data_Explain=RT_NULL;//解释遥控发过来的数据
+	extern motor car;
 	extern Display D1;
 	extern Display D2;
 	extern motor motor2;
 	extern  motor motor1;
+	static rt_sem_t get_data=RT_NULL;//创建一个信号量,用于正确接收数据后进行判断
+	
+	Usart_get getdata;
 	rt_uint8_t PID_Para[16];//串口2获得的PID参数
 	rt_uint8_t Dis_Para[30];//串口3获得屏幕发过来的数据
 	rt_uint8_t Dis_Para_Copy[30];//复制串口3获得的数据
@@ -89,54 +96,9 @@
 		
 		
 	}
-//	JISUAN Motor2_Sf;
-//	void PID_Para_thread_entry(void* parameter)
-//{    
-//   
-//		JISUAN P;
-//		JISUAN I;
-//		JISUAN D;
-//		rt_uint8_t i=0;			
-//		 while (1)
-//	{   
-//		
 
-//		for(i=0;i<16;i++)
-//		{
-//					PID_Para[i]=uart2_getchar();//在里面已经包含有调度函数，当没有数据输入时会停留在这一步，时间到后调度到其他任务
-//					if(PID_Para[0]!=0xAA)
-//					{
-//						i=0;
-//						break;//一定要使用break，不然循环逻辑会出现问题
-//					}
-//				
-//		}
-//		if(PID_Para[1]==0x07)
-//		{
-//			P.x[0]=PID_Para[2];
-//			P.x[1]=PID_Para[3];
-//			P.x[2]=PID_Para[4];
-//			P.x[3]=PID_Para[5];
-//			Motor2_PID.P_PID=P.f;
-//			I.x[0]=PID_Para[6];
-//			I.x[1]=PID_Para[7];
-//			I.x[2]=PID_Para[8];
-//			I.x[3]=PID_Para[9];
-//			Motor2_PID.I_PID=I.f;
-//			D.x[0]=PID_Para[10];
-//			D.x[1]=PID_Para[11];
-//			D.x[2]=PID_Para[12];
-//			D.x[3]=PID_Para[13];
-//			Motor2_PID.D_PID=D.f;
-//			PID_Para[1]=0xff;
-//			
-//		}
-//		rt_thread_delay(30);	
-//		
-//	}
-//}
 
-	void Usart3_Parameter_Control(void* parameter)
+	void Usart3_Parameter_Get(void* parameter)
 {    
     rt_uint8_t i;
 		uint8_t b;
@@ -155,17 +117,36 @@
 						break;
 				}
 			
-				
-		
-//					if(Dis_Para[i-1]==0xff|i>30|Dis_Para[0]!=0xEE)
-//					{
-//						b=i-1;
-//						i=0;
-//						
-//					}
-				if(i==12)
+				if(Dis_Para[i]==0xff&Dis_Para[i-1]==0xff)
 				{
-							if(Dis_Para[0]==0xee&&Dis_Para[5]==0xC6)//判断是否接收已经结束
+					getdata.i=i-4;//记录获得数据的个数
+					for(b=0;b<i;b++)
+					{
+						getdata.data[b]=Dis_Para[b];//获得数据
+						
+					}
+					rt_sem_release(get_data);//释放信号量，使得数据可以在下一个线程中
+					break;//跳出for(i=0;i<30;i++)的循环
+					
+				}
+				
+			}			
+	}
+}
+
+
+void Parameter_Exlpain(void *parameter)
+{
+	uint8_t Ascii[15];//用来储存要变为整形的ASCII
+	uint8_t j=0;
+	while(1)
+	{
+		
+		rt_sem_take(get_data,RT_WAITING_FOREVER);//等待获取遥控串口线束后释放的信号
+		
+			if(getdata.data[10]==0x0f)
+			{
+							if(getdata.data[4]==0xC6&&getdata.data[5]==0xC6)//判断是否接收已经结束
 							{
 											
 										
@@ -180,7 +161,7 @@
 
 							}
 					
-							if(Dis_Para[0]==0xee&&Dis_Para[5]==0xC5)//判断是否接收已经结束
+							if(getdata.data[4]==0xC5&&getdata.data[5]==0xC5)//判断是否接收已经结束
 							{
 											
 											
@@ -194,42 +175,71 @@
 												PWM_Story=10;
 											}
 										
-
 							}
-						break;	
-				}
-			}			
+			}
+			if(getdata.data[4]==0x01)//第一个页面发过来的数据
+			{
+					if(getdata.data[6]==13)//得到第一个角度
+					{
+							for(j=8;j<getdata.i;j++)
+							{
+								Ascii[j-8]=getdata.data[j];
+								
+							}
+							car.Angle= Get_AutoRun(Ascii,getdata.i-8);//把大彩串口屏的ASCII数据转换为浮点数;J为ASCII个数加1，KK为数组
+							
+					}
+				
+				
+			}
+		}
+		
+		
 	}
-}
+	
+	
+	
+
 /*******任务初始化********/
 int PID_Test_Init(void)
 {
-   rt_thread_t PID_Get;//获取PID参数设置的任务
-	rt_thread_t PID_Send;//往电脑中发送速度信息
+  
+	get_data=rt_sem_create("get_data",1,RT_IPC_FLAG_FIFO);//初始化信号量
 		uart1_open("uart1");
 		
 				
 	
 			
     /* 创建test线程 */
-    PID_Get = rt_thread_create("PID_GET",
+   Angle_Get_Data = rt_thread_create("Angle_Get_Data",
                     Angle_Get, 
                     RT_NULL,
                     1024, 
-										11, 
+										21, 
                     10);
+	if (Angle_Get_Data!= RT_NULL)
+        rt_thread_startup(Angle_Get_Data);
 	
-		PID_Send=rt_thread_create("PID_DIS",
-													Usart3_Parameter_Control,
+		Usart_Remote_Data=rt_thread_create("Usart_Remote_Data",
+													Usart3_Parameter_Get,
 													RT_NULL,
 													1024,
-													10,
+													20,
 													10);
-//    /* 创建成功则启动线程 */
-    if (PID_Get!= RT_NULL)
-        rt_thread_startup(PID_Get);
-    if (PID_Send!= RT_NULL)
-        rt_thread_startup(PID_Send);    
+
+	if (Usart_Remote_Data!= RT_NULL)
+				rt_thread_startup(Usart_Remote_Data);
+	
+	
+		Data_Explain=rt_thread_create("Data_Explain",
+													Parameter_Exlpain,
+													RT_NULL,
+													1024,
+													19,
+													10);
+       
+		if (Data_Explain!= RT_NULL)
+						rt_thread_startup(Data_Explain);
     return 0;
 }
 /* 如果设置了RT_SAMPLES_AUTORUN，则加入到初始化线程中自动运行 */
